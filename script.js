a52d0d9e5d77f8480d3306c15f670a2188ea194c
Arkhavy <arkhavy@gmail.com>
2026-08-31 18:48:12 +0200

    [REFACTOR] Refactor whole logic: Board creation
    Addition of the createGameBoard factory function
    Not finished yet, but a start is there
    Some functions and variables might be deleted / changed later

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
+let gameBoardCount = 0;
+function createGameBoard() {
+	const gameBoardNumber = gameBoardCount;
+	gameBoardCount++;
+
+	const id = crypto.randomUUID();
+	const gameBoard = ["", "", "", "", "", "", "", "", ""];
+	let gameState = "waiting"; // waiting | ongoing | win P1 | win P2 | tie
+	let player1 = null;
+	let player2 = null;
+
+	const getId = () => { return (id); };
+	const getGameBoardNumber = () => { return (gameBoardNumber); };
+	const getGameBoard = () => { return (gameBoard); };
+	const getGameState = () => { return (gameState); };
+	const getPlayer1 = () => { return (player1); };
+	const getPlayer2 = () => { return (player2); };
+	const getTile = (index) => { return (gameBoard[index]); };
+
+	const setTile = (symbol, index) => { gameBoard[index] = symbol; };
+	const setPlayer1 = (player) => { player1 = player; };
+	const setPlayer2 = (player) => { player2 = player; };
+
+	return ({
+		getId, getGameBoardNumber,
+		getGameBoard, getGameState,
+		getPlayer1, getPlayer2,
+		setPlayer1, setPlayer2
+	});
+}
+
 function createPlayer() {
+	const id = crypto.randomUUID();
 	let name = "";
 	let win = 0;
 	let lose = 0;
 	let tie = 0;
 	let symbol = "";
 
+	const getId = () => { return (id); };
 	const getSymbol = () => { return (symbol); };
 	const getName = () => { return (name); };
 	const getWin = () => { return (win); };
@@ -18,6 +51,7 @@ function createPlayer() {
 	const incrementTie = () => { tie++; };
 
 	return ({
+		getId,
 		getSymbol, getName,
 		getWin, getLose,
 		getTie, setSymbol,
@@ -29,8 +63,11 @@ function createPlayer() {
 /* ************************************************************************** */
 /*                                    TEST                                    */
 /* ************************************************************************** */
+
+/* ******************************* PLAYER TEST ****************************** */
 function displayPlayer(player) {
 	console.log(player);
+	console.log(`id: ${player.getId()}`);
 	console.log(`symbol: ${player.getSymbol()}`);
 	console.log(`name: ${player.getName()}`);
 	console.log(`win: ${player.getWin()}`);
@@ -62,3 +99,33 @@ playerOne.incrementTie();
 
 displayPlayer(playerOne);
 displayPlayer(playerTwo);
+
+/* ***************************** GAMEBOARD TEST ***************************** */
+function displayGameBoard(gameBoard) {
+	console.warn(gameBoard);
+	console.log(`ID: ${gameBoard.getId()}`);
+	console.log(`GameBoardNumber: ${gameBoard.getGameBoardNumber()}`);
+	console.log(`GameBoard: ${gameBoard.getGameBoard()}`);
+	console.log(`GameState: ${gameBoard.getGameState()}`);
+	if (gameBoard.getPlayer1()) {
+		console.log(`player1: ${gameBoard.getPlayer1().getName()}`);
+	} else {
+		console.log("No player 1 yet.");
+	}
+	if (gameBoard.getPlayer2()) {
+		console.log(`player2: ${gameBoard.getPlayer2().getName()}`);
+	} else {
+		console.log("No player 2 yet.");
+	}
+}
+
+const gameBoardOne = createGameBoard();
+const gameBoardTwo = createGameBoard();
+const gameBoardThree = createGameBoard();
+
+gameBoardOne.setPlayer1(playerOne);
+gameBoardOne.setPlayer2(playerTwo);
+
+displayGameBoard(gameBoardOne);
+displayGameBoard(gameBoardTwo);
+displayGameBoard(gameBoardThree);
